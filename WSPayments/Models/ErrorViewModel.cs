5422cd1dd8f1b48d2be0ded5747c7293f2de347c
David Romo <dromo@itexico.com>
2020-07-06 15:15:00 -0500

    Add project files.

---
+++ WSPayments/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WSPayments.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
